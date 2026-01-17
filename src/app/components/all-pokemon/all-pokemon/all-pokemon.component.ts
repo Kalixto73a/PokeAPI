ee1fbf70b144c39302cce0022fb096d9446666fe
Kalixto73a <Kalixto75@gmail.com>
2026-01-17 03:05:21 +0100

    Feat: Create All Pokemon Component

---
+++ src/app/components/all-pokemon/all-pokemon/all-pokemon.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { AllPokemonAPICallService } from '../../../services/AllPokemonAPICall/all-pokemon-apicall.service';
+
+
+@Component({
+  selector: 'app-all-pokemon',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule,],
+  providers: [AllPokemonAPICallService],
+  templateUrl: './all-pokemon.component.html',
+  styleUrl: './all-pokemon.component.css'
+})
+
+export class AllPokemonComponent implements OnInit {
+
+  pokemons: any[] = [];
+  limit = 20;
+  offset = 0;
+  loading = false;
+
+  constructor(private allPokemonService: AllPokemonAPICallService) {}
+
+  ngOnInit(): void {
+    this.loadPokemons();  
+  }
+
+  loadPokemons(): void {
+    this.loading = true;
+    this.allPokemonService.getAllPokemon(this.limit, this.offset)
+      .subscribe({
+        next: response => {
+          this.pokemons = response.results;
+          this.loading = false;
+        },
+        error: () => this.loading = false
+      });
+  }
+
+  netxPage(): void {
+    this.offset += this.limit;
+    this.loadPokemons();
+  }
+
+  prevPage(): void {
+    if (this.offset === 0) return;
+    this.offset -= this.limit;
+    this.loadPokemons();
+  }
+
+  
+}
