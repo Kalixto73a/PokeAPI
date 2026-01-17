75eaffde6af9b43c76c940cd0fcf57783a893835
Kalixto73a <Kalixto75@gmail.com>
2026-01-17 03:08:06 +0100

    Refactor: Add basic structure and AllPokemonComponent to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AllPokemonComponent } from './components/all-pokemon/all-pokemon/all-pokemon.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, AllPokemonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
