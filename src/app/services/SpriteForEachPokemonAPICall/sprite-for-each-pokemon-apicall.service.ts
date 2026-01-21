0cfb9d081e2eeaf055cdb3943c432a54b5cc37f8
Kalixto73a <Kalixto75@gmail.com>
2026-01-21 05:10:14 +0100

    Refactor: Type correctly and create sprite-for-each-pokemon-apicall model

--- src/app/services/SpriteForEachPokemonAPICall/sprite-for-each-pokemon-apicall.service.ts
+++ src/app/services/SpriteForEachPokemonAPICall/sprite-for-each-pokemon-apicall.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'; 
+import { PokemonSprite } from '../../model/sprite-for-each-pokemon-apicall/sprite-for-each-pokemon-apicall';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpriteForEachPokemonApicallService {
-  privateUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  private  baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  
   constructor(private http: HttpClient) { }
 
-  getSpriteForPokemon(id: string): Observable<any> {
-    return this.http.get(`${this.privateUrl}${id}`);
+  getSpriteForPokemon(id: string | number): Observable<PokemonSprite> {
+    return this.http.get<PokemonSprite>(`${this.baseUrl}${id}`);
   }
 }
