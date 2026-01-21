9a7d1ef61723ff66df0756a0d2100ff831a6ccb8
Kalixto73a <Kalixto75@gmail.com>
2026-01-21 05:10:14 +0100

    Refactor: Type correctly and create sprite-for-each-pokemon-apicall model

---
+++ src/app/model/sprite-for-each-pokemon-apicall/sprite-for-each-pokemon-apicall.ts
@@ -0,0 +1,13 @@
+export class PokemonSprite{
+
+    public id : string
+    public name : string
+    public sprites  :  DiferentSprites
+    
+}
+
+export class DiferentSprites{
+
+    public front_default : string
+
+}
