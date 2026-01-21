b183c9759433f72be3ecd07a783753ebf0abffb8
Kalixto73a <Kalixto75@gmail.com>
2026-01-21 04:48:52 +0100

    Refactor: Type correctly, update Css and create the all-pokemon model

---
+++ src/app/model/all-pokemon/all-pokemon.ts
@@ -0,0 +1,16 @@
+export class AllPokemon {
+
+    private count : number 
+    private next : string
+    private previous : boolean
+
+    public results : Pokemon[]
+
+}
+
+export class Pokemon {
+
+    public name : string
+    private url : string
+    
+}
