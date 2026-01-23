2047a21c32a7df82cec99a24e20c74113e9f87da
Kalixto73a <Kalixto75@gmail.com>
2026-01-23 11:14:17 +0100

    Fix: Change the Services URL to the environment.ts

--- src/app/services/all-pokemon-apicall/all-pokemon-apicall.service.ts
+++ src/app/services/all-pokemon-apicall/all-pokemon-apicall.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AllPokemon } from '../../model/all-pokemon/all-pokemon';
 import { PokemonQuery } from '../../model/all-pokemon-apicall/all-pokemon-apicall';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,12 @@ import { PokemonQuery } from '../../model/all-pokemon-apicall/all-pokemon-apical
 
 export class AllPokemonAPICallService {
 
-  private baseURL = 'https://pokeapi.co/api/v2';
-
   constructor(private http: HttpClient) { }
 
   getAllPokemon(query: PokemonQuery): Observable<AllPokemon> {
     const { limit, offset } = query
     return this.http.get<AllPokemon>(
-      `${this.baseURL}/pokemon?limit=${limit}&offset=${offset}`
+      `${environment.apiUrl}/pokemon?limit=${limit}&offset=${offset}`
     );
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  API_URL: 'https://pokeapi.co/api/v2/'
+  apiUrl: 'https://pokeapi.co/api/v2/'
 };
