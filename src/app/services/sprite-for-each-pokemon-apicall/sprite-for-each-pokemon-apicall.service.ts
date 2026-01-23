8a96f855d13a3e256ed3c5fc961e6abf4eadf438
Kalixto73a <Kalixto75@gmail.com>
2026-01-23 11:14:17 +0100

    Fix: Change the Services URL to the environment.ts

--- src/app/services/sprite-for-each-pokemon-apicall/sprite-for-each-pokemon-apicall.service.ts
+++ src/app/services/sprite-for-each-pokemon-apicall/sprite-for-each-pokemon-apicall.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'; 
 import { PokemonSprite } from '../../model/sprite-for-each-pokemon-apicall/sprite-for-each-pokemon-apicall';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class SpriteForEachPokemonApicallService {
-  private  baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
   
   constructor(private http: HttpClient) { }
 
   getSpriteForPokemon(id: string | number): Observable<PokemonSprite> {
-    return this.http.get<PokemonSprite>(`${this.baseUrl}${id}`);
+    return this.http.get<PokemonSprite>(`${environment.apiUrl}/pokemon/${id}`);
   }
 }
