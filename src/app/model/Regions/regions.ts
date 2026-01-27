5b31b3b323cf8d0a15f8d8434bc0388af2d9c362
Kalixto73a <Kalixto75@gmail.com>
2026-01-27 13:54:40 +0100

    Feat: Create Regions model

---
+++ src/app/model/Regions/regions.ts
@@ -0,0 +1,17 @@
+export class Regions{
+
+  private count : number
+  private next: boolean
+  private previous: boolean
+
+  public  results : RegionDetails[]
+
+}
+
+export class RegionDetails {
+  
+  public name: string
+
+  private url: string
+
+}
