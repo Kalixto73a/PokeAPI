6358fa450435147aa014041fc5635eba2eae0fb0
Kalixto73a <Kalixto75@gmail.com>
2026-01-28 18:09:44 +0100

    Feat: Create the .ts for the images of the regions

---
+++ src/app/core/config/regions-list-images.ts
@@ -0,0 +1,12 @@
+export const RegionImages: Record<string, string> = {
+    kanto: 'https://res.cloudinary.com/dzfqdntdw/image/upload/v1769614190/285px-Mapa_de_Kanto_LGPE_aj1akw.png',
+    johto: 'https://res.cloudinary.com/dzfqdntdw/image/upload/v1769615230/285px-Johto_HGSS_zndqlj.png',
+    hoenn: 'https://res.cloudinary.com/dzfqdntdw/image/upload/v1769615230/285px-Mapa_de_Hoenn_ROZA_rv0pct.png',
+    sinnoh: 'https://res.cloudinary.com/dzfqdntdw/image/upload/v1769615230/285px-Sinnoh_DBPR_tzgrbc.png',
+    unova: 'https://res.cloudinary.com/dzfqdntdw/image/upload/v1769615229/285px-Teselia_N2B2_bn6zov.png',
+    kalos: 'https://res.cloudinary.com/dzfqdntdw/image/upload/v1769615229/285px-Mapa_Kalos_fnx1i6.png',
+    alola: 'https://res.cloudinary.com/dzfqdntdw/image/upload/v1769615229/285px-Alola_USUL_wee0nz.png',
+    galar: 'https://res.cloudinary.com/dzfqdntdw/image/upload/v1769615230/285px-Galar_chdqcx.png',
+    hisui: 'https://res.cloudinary.com/dzfqdntdw/image/upload/v1769616400/7bef56b0-292a-44a7-a195-9b66561377de.png',
+    paldea: 'https://res.cloudinary.com/dzfqdntdw/image/upload/v1769615230/285px-Paldea_tf3zxl.jpg',
+}
