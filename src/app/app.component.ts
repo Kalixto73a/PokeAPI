01df0a502d82f927eee8502b0aaf1deacd29e789
Kalixto73a <Kalixto75@gmail.com>
2026-01-30 15:29:24 +0100

    Refactor: Update and add RegionsListComponent and delete all-pokemonComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AllPokemonComponent } from './components/all-pokemon/all-pokemon.component';
 import { RegionsListComponent } from './components/regions-list/regions-list.component';
+import { PokedexForEachRegionComponent } from './components/pokedex-for-each-region/pokedex-for-each-region.component';
+import { HttpClientModule } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, AllPokemonComponent, RegionsListComponent],
+  imports: [RouterOutlet, CommonModule, RegionsListComponent, HttpClientModule, PokedexForEachRegionComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'PokeAPI';
+
+  ngOnInit(): void{
+    Swal.fire({
+          icon: 'info',
+          title: 'Welcome',
+          html: 'Please select a region to see his pokemons.',
+          theme: 'dark',
+          confirmButtonText: 'Okey',
+          customClass: {
+            confirmButton: 'bg-[#FFD700] text-black hover:bg-[#DAA520]'
+          },
+        })
+  }
 }
