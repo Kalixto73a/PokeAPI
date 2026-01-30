7684970ceb00a101a13654194630376e28925075
Kalixto73a <Kalixto75@gmail.com>
2026-01-30 15:54:16 +0100

    Feat: Create RegionDeatailsAPICall Service

---
+++ src/app/services/region-details-apicall/region-details-apicall.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RegionDetails, Pokedexes } from '../../model/Regions/region-details';
+import { environment } from '../../../environments/environment';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegionDetailsAPICallService {
+
+  constructor( private http: HttpClient) { }
+
+   public getRegionDetails(id: number): Observable<Pokedexes> {
+      return this.http.get<RegionDetails>(`${environment.apiUrl}/region/${id}`).pipe(map(response => response.pokedexes[0]))
+    }
+
+}
