4fe1b4f19a0a2deb1f9300ba4b38eae676c43a58
Kalixto73a <Kalixto75@gmail.com>
2026-01-31 00:30:23 +0100

    Refactor: Update and add routes to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { RegionsListComponent } from './components/regions-list/regions-list.component';
+import { HeaderComponent } from './components/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RegionsListComponent, HttpClientModule, ],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, HttpClientModule, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
