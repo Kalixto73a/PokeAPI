7cdc9b617c4d067fc50220be6c53c2d6671915f2
Kalixto73a <Kalixto75@gmail.com>
2026-01-31 00:22:43 +0100

    Feat: Create Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegionsListComponent } from './components/regions-list/regions-list.component';
+import { PokedexForEachRegionComponent } from './components/pokedex-for-each-region/pokedex-for-each-region.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: RegionsListComponent,
+    },
+    {
+        path: 'region/:id',
+        component: PokedexForEachRegionComponent
+    }
+];
