863e07271cb52175bad9625103a9b5e1686c99cd
Kalixto73a <Kalixto75@gmail.com>
2026-02-01 15:49:23 +0100

    Refactor: Change & update model sprite-for-each-pokemon to pokemon-details

--- src/app/services/sprite-for-each-pokemon-apicall/sprite-for-each-pokemon-apicall.service.ts
+++ src/app/services/sprite-for-each-pokemon-apicall/sprite-for-each-pokemon-apicall.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'; 
-import { PokemonSprite } from '../../model/Sprites/sprite-for-each-pokemon-apicall';
+import { PokemonSprite } from '../../model/Pokemons/pokemon-details';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
