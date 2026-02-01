1a2a6347b35ee15c78b4d9d338c2658d1eacd27e
Kalixto73a <Kalixto75@gmail.com>
2026-02-01 20:52:10 +0100

    Feat: Create pokemon-details component

---
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -0,0 +1,104 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonDetails } from '../../model/Pokemons/pokemon-details';
+import { PokemonNumberComponent } from '../pokemon-number/pokemon-number.component';
+import { PokemonTypesComponent } from '../pokemon-types/pokemon-types.component';
+import { DetailsForEachPokemonApicallService } from '../../services/pokemon-details/pokemon-details-apicall.service';
+import { SpriteForEachPokemonComponent } from '../sprite-for-each-pokemon/sprite-for-each-pokemon.component';
+import { Pokemon } from '../../model/Pokemons/pokedex';
+import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-pokemon-details',
+  standalone: true,
+  imports: [ PokemonNumberComponent, PokemonTypesComponent, SpriteForEachPokemonComponent, CommonModule ],
+  providers: [ DetailsForEachPokemonApicallService ],
+  templateUrl: './pokemon-details.component.html',
+  styleUrl: './pokemon-details.component.css'
+})
+
+export class PokemonDetailsComponent implements OnInit{
+
+  @Input() public pokemonId : number
+
+  public loading: boolean
+  public pokemon: PokemonDetails
+
+  /**
+   * 
+   * Constructor
+   * 
+   * @param {DetailsForEachPokemonApicallService} detailsForEachPokemonService
+   * 
+   */
+
+  constructor(    
+
+    private detailsForEachPokemonService: DetailsForEachPokemonApicallService,
+    private route: ActivatedRoute,
+
+  ) {}
+
+  public ngOnInit(): void{
+
+    this.initializeValues()
+
+  }
+
+  private initializeValues(): void{
+
+    this.getIdByUrl(),
+    this.loadPokemonDetails()
+    
+  } 
+  
+  private getIdByUrl(): void {
+    this.loading = true
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id')
+      if (id) {
+        this.pokemonId = +id
+        this.loading= false
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          html: 'An Error Ocurred<br><br>Please reload the page and try again.',
+          theme: 'dark',
+          confirmButtonText: 'Retry',
+          confirmButtonColor: '#FF0000',
+        }).then((result) => {
+         if (result.isConfirmed) {
+            window.location.reload();
+          }
+        })
+      }
+    })
+  }
+  
+  private loadPokemonDetails(): void{
+    this.loading = true
+    if (this.pokemonId) {
+      this.detailsForEachPokemonService.getDetailsOfPokemon(this.pokemonId)
+        .subscribe(details => {
+          this.pokemon = details
+          this.loading = false
+        }, error => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            html: 'An Error Ocurred<br><br>Please reload the page and try again.',
+            theme: 'dark',
+            confirmButtonText: 'Retry',
+            confirmButtonColor: '#FF0000',
+          }).then((result) => {
+          if (result.isConfirmed) {
+              window.location.reload();
+            }
+          })
+        })
+    }
+  }
+
+}
