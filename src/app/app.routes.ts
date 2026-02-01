a5e18ade7f8eb122fb118544b436f4f1fde005fc
Kalixto73a <Kalixto75@gmail.com>
2026-02-01 20:57:09 +0100

    Refactor: Add PokemonDetails route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { RegionsListComponent } from './components/regions-list/regions-list.component';
 import { PokedexForEachRegionComponent } from './components/pokedex-for-each-region/pokedex-for-each-region.component';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'region/:id',
         component: PokedexForEachRegionComponent
+    },
+    {
+        path: 'pokemon/:id',
+        component:  PokemonDetailsComponent
     }
 ];
