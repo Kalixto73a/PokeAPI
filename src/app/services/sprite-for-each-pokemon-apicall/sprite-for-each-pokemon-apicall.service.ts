3b2d0738a88e4be7bd48513e09a54be96b6f6793
Kalixto73a <Kalixto75@gmail.com>
2026-02-01 16:06:53 +0100

    Refactor: Update sprite-for-each-pokemon-apicall Service

--- src/app/services/sprite-for-each-pokemon-apicall/sprite-for-each-pokemon-apicall.service.ts
+++ src/app/services/sprite-for-each-pokemon-apicall/sprite-for-each-pokemon-apicall.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'; 
-import { PokemonSprite } from '../../model/Pokemons/pokemon-details';
+import { PokemonDetails } from '../../model/Pokemons/pokemon-details';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -12,7 +12,7 @@ export class SpriteForEachPokemonApicallService {
   
   constructor(private http: HttpClient) { }
 
-  public getSpriteForPokemon(id: string | number): Observable<PokemonSprite> {
-    return this.http.get<PokemonSprite>(`${environment.apiUrl}/pokemon/${id}`);
+  public getSpriteForPokemon(id: string | number): Observable<PokemonDetails> {
+    return this.http.get<PokemonDetails>(`${environment.apiUrl}/pokemon/${id}`);
   }
 }
