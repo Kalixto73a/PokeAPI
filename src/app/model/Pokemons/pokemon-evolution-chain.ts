85a4ebf30da1112c3dcbbcdd314e6f3cab0f174a
Kalixto73a <Kalixto75@gmail.com>
2026-02-03 15:06:48 +0100

    Feat: Create evolution-chain Model

---
+++ src/app/model/Pokemons/pokemon-evolution-chain.ts
@@ -0,0 +1,46 @@
+export class EvolutionChain {
+
+  public id: number       
+  public chain: EvolutionChainOfPokemon;
+
+}
+
+export class EvolutionChainOfPokemon {
+
+    public evolution_details: EvolutionDetails[]
+    public evolves_to: EvolutionChainOfPokemon[]
+    public species: NamedAPIResource
+}
+
+export class EvolutionDetails {
+
+  public gender?: number | null
+  public held_item?: NamedAPIResource | null
+  public item?: NamedAPIResource | null
+  public known_move?: NamedAPIResource | null
+  public known_move_type?: NamedAPIResource | null
+  public location?: NamedAPIResource | null
+  public min_affection?: number | null
+  public min_beauty?: number | null
+  public min_happiness?: number | null
+  public min_level?: number | null
+  public min_move_count?: number | null
+  public min_steps?: number | null
+  public needs_overworld_rain?: boolean | null
+  public needs_multiplayer?: boolean | null
+  public party_species?: NamedAPIResource | null
+  public party_type?: NamedAPIResource | null
+  public time_of_day?: string | null
+  public trade_species?: NamedAPIResource | null
+  public turn_upside_down?: boolean | null
+  public trigger?: NamedAPIResource | null
+  public used_move?: NamedAPIResource | null
+  
+}
+
+export class NamedAPIResource {
+
+    public name: string
+    public url : string
+
+}
