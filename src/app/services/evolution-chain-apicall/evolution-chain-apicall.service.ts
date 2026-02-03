152999d4e16d84b39347d11b4cffb43f8685cbf9
Kalixto73a <Kalixto75@gmail.com>
2026-02-03 14:37:56 +0100

    Feat: Create EvolutionChainService

---
+++ src/app/services/evolution-chain-apicall/evolution-chain-apicall.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { PokemonSpicies } from '../../model/Pokemons/pokemon-species';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EvolutionChainApicallService {
+
+  constructor( private http: HttpClient) { }
+
+  public getPokemonSpeciesData(url: string): Observable<PokemonSpicies> {
+      return this.http.get<PokemonSpicies>(`${url}`)
+    }
+
+}
