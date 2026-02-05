f00a809ef121f2d3e138779704e8213b22337375
Kalixto73a <Kalixto75@gmail.com>
2026-02-05 18:13:12 +0100

    Feat: Create pokedex-index config

---
+++ src/app/core/config/pokedex-index.ts
@@ -0,0 +1,12 @@
+export const RegionPokedexIndex: Record<number, number[]> = {
+  1: [1],
+  2: [1],
+  3: [1],
+  4: [1],
+  5: [1],
+  6: [ 0,1,2 ],
+  7: [0],
+  8: [ 0,1,2 ],
+  9: [0],
+  10: [ 0,1,2 ],
+};
