40f25bd1947c9d03e29cba94472abafc17425a27
Kalixto73a <Kalixto75@gmail.com>
2026-02-15 07:04:25 +0100

    Refactor: Update pokemon-species.ts config

--- src/app/model/Pokemons/pokemon-species.ts
+++ src/app/model/Pokemons/pokemon-species.ts
@@ -1,11 +1,13 @@
 export class PokemonSpicies {
 
     public evolution_chain: NamedAPIResource
+    public generation: NamedAPIResource
 
 }
 
 export class NamedAPIResource {
 
+    public name: string
     public url: string
 
 }
