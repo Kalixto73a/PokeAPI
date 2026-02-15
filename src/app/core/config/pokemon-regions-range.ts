cef143840f99ceeef4c4996632557accea258872
Kalixto73a <Kalixto75@gmail.com>
2026-02-15 07:02:33 +0100

    Feat: Create pokemon-regions-range.ts config

---
+++ src/app/core/config/pokemon-regions-range.ts
@@ -0,0 +1,12 @@
+export const PokemonRegionRanges: { regionId: number; range: number[] | [number, number][] }[] = [
+  { regionId: 1, range: [[1,151]] },
+  { regionId: 2, range: [[152,251]] },
+  { regionId: 3, range: [[252,386]] },
+  { regionId: 4, range: [[387,493]] },
+  { regionId: 5, range: [[494,649]] },
+  { regionId: 6, range: [[650,721]] },
+  { regionId: 7, range: [[722,809]] },
+  { regionId: 9, range: [899,900,901,902,903,904] },
+  { regionId: 8, range: [[810,905]] },
+  { regionId: 10, range: [[906,1025]] },
+]
