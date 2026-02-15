26a2d583087e893b1473bbb19180762965ae2640
Kalixto73a <Kalixto75@gmail.com>
2026-02-15 19:09:27 +0100

    Refactor: Add null case for the DiferentSprites class

--- src/app/model/Pokemons/pokemon-details.ts
+++ src/app/model/Pokemons/pokemon-details.ts
@@ -12,14 +12,14 @@ export class PokemonDetails{
 
 export class DiferentSprites{
 
-    public front_default : string
-    public front_female: string
-    public front_shiny: string
-    public front_shiny_female: string
-    public back_default: string
-    public back_female: string
-    public back_shiny: string
-    public back_female_shiny: string
+    public front_default : string | null
+    public front_female: string | null
+    public front_shiny: string | null
+    public front_shiny_female: string | null
+    public back_default: string | null
+    public back_female: string | null
+    public back_shiny: string | null
+    public back_shiny_female: string | null
 
 }
 
