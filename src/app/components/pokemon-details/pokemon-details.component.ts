ad68b028862beb074d8ad1e1888902cb5f4f1f66
Kalixto73a <Kalixto75@gmail.com>
2026-02-15 06:55:59 +0100

    Refactor: Refactor & change regionName&regionImage to originRegionName&originRegionImage

--- src/app/components/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -5,21 +5,21 @@ import { PokemonNumberComponent } from '../pokemon-number/pokemon-number.compone
 import { PokemonTypesComponent } from '../pokemon-types/pokemon-types.component';
 import { DetailsForEachPokemonApicallService } from '../../services/pokemon-details/pokemon-details-apicall.service';
 import { SpriteForEachPokemonComponent } from '../sprite-for-each-pokemon/sprite-for-each-pokemon.component';
-import { Pokemon } from '../../model/Pokemons/pokedex';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { PokemonEvolutionChainComponent } from '../pokemon-evolution-chain/pokemon-evolution-chain.component';
 import Swal from 'sweetalert2';
-import { RegionStateService } from '../../services/region-state-service/region-state.service';
 import { PokemonTypesColors } from '../../core/config/types-colors';
-import { RegionDetailsAPICallService } from '../../services/region-details-apicall/region-details-apicall.service';
+import { RegionNames } from '../../core/config/regions-names';
 import { RegionImages } from '../../core/config/regions-list-images';
+import { PokemonSpeciesApicallService } from '../../services/pokemon-species-apicall/pokemon-species-apicall.service';
+import { GenerationToRegionId } from '../../core/config/generation-to-region';
 
 @Component({
   selector: 'app-pokemon-details',
   standalone: true,
   imports: [ HttpClientModule, PokemonNumberComponent, PokemonTypesComponent, SpriteForEachPokemonComponent, CommonModule, PokemonEvolutionChainComponent ],
-  providers: [ DetailsForEachPokemonApicallService, RegionDetailsAPICallService ],
+  providers: [ DetailsForEachPokemonApicallService, PokemonSpeciesApicallService ],
   templateUrl: './pokemon-details.component.html',
   styleUrl: './pokemon-details.component.css'
 })
@@ -28,11 +28,14 @@ export class PokemonDetailsComponent implements OnInit{
 
   @Input() public pokemonId : number
 
+  private readonly hisuiPokemonIds = [899, 900, 901, 902, 903, 904]
+  
   public loading: boolean
   public pokemon: PokemonDetails
   public regionId: number | null 
-  public regionImage: Record<number, string>
-  public regionName: string | null
+  public originRegionId: number | null
+  public originRegionName: string | null
+  public originRegionImage: string | null
   public statsBackground: string
   public statsWidths: number[]
 
@@ -41,7 +44,6 @@ export class PokemonDetailsComponent implements OnInit{
    * Constructor
    * 
    * @param {DetailsForEachPokemonApicallService} detailsForEachPokemonService
-   * @param {RegionStateService} regionState
    * @param {RegionDetailsAPICallService} regionDetailsService
    * 
    */
@@ -49,8 +51,7 @@ export class PokemonDetailsComponent implements OnInit{
   constructor(    
 
     private detailsForEachPokemonService: DetailsForEachPokemonApicallService,
-    private regionState: RegionStateService,
-    private regionDetailsService: RegionDetailsAPICallService,
+    private pokemonSpeciesService: PokemonSpeciesApicallService,
     private route: ActivatedRoute,
     private router: Router, 
 
@@ -67,7 +68,9 @@ export class PokemonDetailsComponent implements OnInit{
     this.loading = false
     this.pokemon = {} as PokemonDetails
     this.regionId = null
-    this.regionImage = RegionImages
+    this.originRegionId = null
+    this.originRegionName = null
+    this.originRegionImage = null
     this.statsBackground = ''
     this.statsWidths = []
     this.getIdByUrl()
@@ -81,7 +84,6 @@ export class PokemonDetailsComponent implements OnInit{
       this.regionId = Number(params.get('id'))
       this.pokemonId = Number(params.get('pokemonId'))
       if (this.regionId && this.pokemonId) {
-        this.getRegionDetails()
         this.loadPokemonDetails()
         this.loading= false
       } else {
@@ -94,46 +96,32 @@ export class PokemonDetailsComponent implements OnInit{
           confirmButtonColor: '#FF0000',
         }).then((result) => {
          if (result.isConfirmed) {
-            window.location.reload();
+            window.location.reload()
           }
         })
       }
     })
   }
   
-  private getRegionDetails(): void {
-    this.loading = true
-    if (this.regionId !== null){
-      this.regionDetailsService.getRegionDetails(this.regionId)
-      .subscribe({
-        next: (response) =>{
-          this.regionName = response.name
-          this.regionId = response.id
-          this.loading = false;
-        },
-        error: () => {
-          Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            html: 'An Error Ocurred<br><br>Please reload the page and try again.',
-            theme: 'dark',
-            confirmButtonText: 'Retry',
-            confirmButtonColor: '#FF0000',
-          }).then((result) => {
-            if (result.isConfirmed) {
-              window.location.reload();
-            }
-          })
-        }
-      })
-    }
-  }
   private loadPokemonDetails(): void{
     this.loading = true
     if (this.pokemonId) {
       this.detailsForEachPokemonService.getDetailsOfPokemon(this.pokemonId)
         .subscribe({next: details => {
           this.pokemon = details
+          if (details?.species?.url) {
+            this.pokemonSpeciesService.getPokemonSpeciesData(details.species.url)
+              .subscribe(speciesData => {
+                const generationName = speciesData.generation?.name
+                  if (this.hisuiPokemonIds.includes(this.pokemonId)) {
+                    this.originRegionId = 9
+                  } else {
+                    this.originRegionId = GenerationToRegionId[generationName] ?? 0
+                  }
+                this.originRegionName = RegionNames[this.originRegionId]
+                this.originRegionImage = RegionImages[this.originRegionId]
+              })
+          }
           this.updateStatsBackground()
           this.animateStats()
           this.loading = false
@@ -147,7 +135,7 @@ export class PokemonDetailsComponent implements OnInit{
             confirmButtonColor: '#FF0000',
           }).then((result) => {
           if (result.isConfirmed) {
-              window.location.reload();
+              window.location.reload()
             }
           })}
       })
@@ -155,40 +143,34 @@ export class PokemonDetailsComponent implements OnInit{
   }
 
   public getWeightInKg(weight: number): string {
-  return (weight / 10).toFixed(1);
+  return (weight / 10).toFixed(1)
   }
   
   public updateStatsBackground() {
     if(this.pokemon?.types){
-      this.statsBackground = this.calculateCardBackground(this.pokemon.types);
+      this.statsBackground = this.calculateCardBackground(this.pokemon.types)
     }
   }
   public calculateCardBackground(types: PokemonTypes[]): string {
-    if (!types || types.length === 0) return '#fff';
-    const colors = types.map(t => PokemonTypesColors[t.type.name] ?? '#777');
-    if (colors.length === 1) return colors[0];
-    return `linear-gradient(135deg, ${colors.join(', ')})`;
+    if (!types || types.length === 0) return '#fff'
+    const colors = types.map(t => PokemonTypesColors[t.type.name] ?? '#777')
+    if (colors.length === 1) return colors[0]
+    return `linear-gradient(135deg, ${colors.join(', ')})`
   }
 
   private animateStats(): void {
-    if (!this.pokemon?.stats) return;
+    if (!this.pokemon?.stats) return
 
-    // primero inicializamos todas las barras a 0
-    this.statsWidths = this.pokemon.stats.map(() => 0);
+    this.statsWidths = this.pokemon.stats.map(() => 0)
 
-    // después de un microtick actualizamos al valor real
     setTimeout(() => {
-      this.statsWidths = this.pokemon.stats.map(s => (s.base_stat / 255) * 100);
-    }, 50); // 50ms de delay suficiente
+      this.statsWidths = this.pokemon.stats.map(s => (s.base_stat / 255) * 100)
+    }, 50)
   }
 
   public goBack(): void {
     const id = this.regionId
     this.router.navigate(['region', id])
-  }
-  
-  public getRegionImage(regionId: number): string{
-      return this.regionImage[regionId]
   }
   
 }
