aba8696c0a28e6099e25fbf506b27954a6276aa8
Kalixto73a <Kalixto75@gmail.com>
2026-02-17 00:29:32 +0100

    Revert "Refactor: Import footer component"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HeaderComponent, HttpClientModule, FooterComponent, ],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, HttpClientModule, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
