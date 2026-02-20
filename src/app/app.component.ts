9d7135c72df3aa3a26d5d5c8d30dc8d0727478e0
Kalixto73a <Kalixto75@gmail.com>
2026-02-21 00:37:29 +0100

    Refactor: Add logic for the scrollToTop button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,12 +8,40 @@ import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HeaderComponent, HttpClientModule, FooterComponent, ],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, HttpClientModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent{
 
-  title = 'PokeAPI';
+  title = 'Pokedex'
+
+  public showScrollButton: boolean =false
+  public buttonBottom: number = 16
+
+  @HostListener('window:scroll', [])
+  @HostListener('window:resize', [])
+
+  public onWindowScroll() {
+    const scrollY = window.scrollY
+    const innerHeight = window.innerHeight
+    const bodyHeight = document.body.scrollHeight
+
+    this.showScrollButton = scrollY > 200
+
+    const footer = document.querySelector('footer')
+    if (footer) {
+      const footerRect = footer.getBoundingClientRect()
+      const overlap = innerHeight - footerRect.top
+      this.buttonBottom = overlap > 0 ? overlap + 16 : 16
+    } else {
+      this.buttonBottom = 16
+    }
+  }
+
+  public scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
 
 }
